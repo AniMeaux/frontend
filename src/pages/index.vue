03c0de1457b85993977e306519df59482bf18249
William DA SILVA <william.da.silva@outlook.com>
2018-09-30 13:23:38 +0200

    Add Teaming

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,6 +22,22 @@
               Grâce à vos dons, financiers ou matériels, aussi infimes soient-ils, nous pouvons élargir notre champ d’action et agir dans les meilleures conditions pour la cause animale.
             </p>
           </section>
+          <section id="teaming">
+            <a href="https://www.teaming.net/ani-meaux" rel="noindex nofollow">
+              <img src="~/assets/img/logo-teaming.png" width="180" alt="Logo Teaming" />
+            </a>
+            <div class="teaming-container">
+              <h2 class="headline">
+                Faire don d'1 € par mois à Ani'Meaux
+              </h2>
+              <p class="body-1">
+                Teaming est une plateforme online pour récolter des fonds destinés à des causes sociales par le biais de micro-dons d'1 € par mois. La philosophie de Teaming se base sur l'idée que seul, un euro n'a pas beaucoup d'utilité, mais que si nous unissons nos efforts, nous pouvons faire de grandes choses.
+              </p>
+              <a href="https://www.teaming.net/ani-meaux" rel="noindex nofollow" class="teaming-button">
+                Rejoindre le groupe Ani'Meaux
+              </a>
+            </div>
+          </section>
           <section id="zooplus">
             <div>
               <a href="https://marketing.net.zooplus.fr/ts/i3971185/tsc?amc=con.zooplus.391263.404652.6883&tst=!!TIMESTAMP!!" title="Profitez de promotions de offres spéciales" target="_blank" rel="nofollow">
@@ -132,6 +148,59 @@
       }
     }
   }
+  
+  #teaming{
+    display: grid;
+    grid-template-columns: 3fr 5fr;
+    grid-gap: 16px;
+    border: 1px solid lightgray;
+
+    & > a{
+      margin: auto;
+    }
+
+    .teaming-container{
+      padding: 16px;
+    }
+
+    .teaming-button{
+      display: inline-block;
+      padding: 6px 12px;
+      margin-bottom: 0;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 1.42857143;
+      text-align: center;
+      white-space: nowrap;
+      vertical-align: middle;
+      -ms-touch-action: manipulation;
+      touch-action: manipulation;
+      cursor: pointer;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      background-image: none;
+      border: 1px solid transparent;
+      border-radius: 4px;
+      
+      color: #fff;
+      background: -moz-linear-gradient(top,#8cd8ff 0,#05abff 100%);
+      background: -webkit-gradient(linear,left top,left bottom,color-stop(0,#8cd8ff),color-stop(100%,#05abff));
+      background: -webkit-linear-gradient(top,#8cd8ff 0,#05abff 100%);
+      background: -o-linear-gradient(top,#8cd8ff 0,#05abff 100%);
+      background: -ms-linear-gradient(top,#8cd8ff 0,#05abff 100%);
+      background: linear-gradient(to bottom,#8cd8ff 0,#05abff 100%);
+      border: 1px solid #0daaff;
+      text-decoration: none;
+      font-size: 18px;
+    }
+
+    @media only screen and (max-width: 783px) {
+      grid-template-columns: 100%;
+    }
+  }
+  
   #zooplus{
     display: grid;
     grid-template-columns: 3fr 5fr;
